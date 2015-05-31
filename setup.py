d1f0ab4bf77bcfb9327b85f3ffd73c2540dce9a2
Netanel Revah <netanelrevah@outlook.com>
2015-05-31 23:20:47 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+__author__ = 'code-museum'
+
+from setuptools import setup
+
+REQUIREMENTS = ['pyzmq']
+
+setup(
+    name='sonrkel-hq',
+    version='0.0.1',
+    py_modules=['bytes'],
+    packages=['_hq'],
+    install_requires=REQUIREMENTS,
+    url='https://github.com/shirasael/snorkel-hq',
+    license='GPLv3',
+    author='code-museum',
+    author_email='code-museum@users.noreply.github.com',
+    description='',
+    keywords=""
+)
