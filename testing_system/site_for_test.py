986dba32c9160604f0b1a22ff508e3d15e0a4851
Netanel Revah <netanelrevah@outlook.com>
2015-06-10 21:47:20 +0300

    add site

--- testing_system/site_for_test.py
+++ testing_system/site_for_test.py
@@ -1,2 +1,9 @@
+from _hq.client import SnorkelHQCommander
+
 __author__ = 'code-museum'
 
+def main():
+    snorkel_hq_commander = SnorkelHQCommander('localhost')
+
+if __name__ == '__main__':
+    main()
