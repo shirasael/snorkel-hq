dd4b78a9876f607e9935b6f4fd916e3754c52ec6
Netanel Revah <netanelrevah@outlook.com>
2015-06-19 17:10:34 +0300

    write nice commander

--- _hq/nice.py
+++ _hq/nice.py
@@ -1,6 +1,7 @@
 __author__ = 'code-museum'
 
 import zmq
+from logbook import error
 
 ZMQ_REQUEST = zmq.REQ
 ZMQ_REPLY = zmq.REP
@@ -49,3 +50,30 @@ class SafeServerZMQSocket(SafeZMQSocket):
     def _initialize(self):
         self._socket = self._context.socket(self._socket_type)
         self._socket.bind(self._address)
+
+
+class Commander(object):
+    STATUS_FIELD = 'status'
+    VALUE_FIELD = 'value'
+
+    def __init__(self, commands_url):
+        self._command_socket = SafeClientZMQSocket(zmq.Context(), ZMQ_REQUEST, commands_url)
+
+    def command(self, command_type, **values):
+        command = {'type': command_type}
+        if values:
+            command.update(values)
+
+        self._command_socket.send_json(command)
+        if not zmq_poll(self._command_socket):
+            error("Timeout while waiting for answer to command: %s" % command_type)
+            self._command_socket.initialize()
+
+        answer = self._command_socket.receive_json()
+        if self.STATUS_FIELD not in answer or self.VALUE_FIELD not in answer:
+            error("Answer format is wrong, use 'success' and 'value' attributes")
+            return None
+        if not answer['success']:
+            error("Command not succeeded, because of %s" % answer['value'])
+            return None
+        return answer['value']
