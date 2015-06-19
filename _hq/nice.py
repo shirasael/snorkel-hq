1ebb99c38bf7d6a88635f5c92668f0d22f0363bf
Netanel Revah <netanelrevah@outlook.com>
2015-06-19 17:22:28 +0300

    write nice commands handler

--- _hq/nice.py
+++ _hq/nice.py
@@ -1,7 +1,7 @@
 __author__ = 'code-museum'
 
 import zmq
-from logbook import error
+from logbook import error, info
 
 ZMQ_REQUEST = zmq.REQ
 ZMQ_REPLY = zmq.REP
@@ -53,16 +53,14 @@ class SafeServerZMQSocket(SafeZMQSocket):
 
 
 class Commander(object):
-    STATUS_FIELD = 'status'
-    VALUE_FIELD = 'value'
+    COMMAND_TYPE_FIELD = 'command_type'
+    PARAMETERS_FIELD = 'parameters'
 
     def __init__(self, commands_url):
         self._command_socket = SafeClientZMQSocket(zmq.Context(), ZMQ_REQUEST, commands_url)
 
-    def command(self, command_type, **values):
-        command = {'type': command_type}
-        if values:
-            command.update(values)
+    def command(self, command_type, parameters):
+        command = {self.COMMAND_TYPE_FIELD: command_type, self.PARAMETERS_FIELD: parameters}
 
         self._command_socket.send_json(command)
         if not zmq_poll(self._command_socket):
@@ -71,9 +69,41 @@ class Commander(object):
 
         answer = self._command_socket.receive_json()
         if self.STATUS_FIELD not in answer or self.VALUE_FIELD not in answer:
-            error("Answer format is wrong, use 'success' and 'value' attributes")
+            error("Answer format is wrong, use 'status' and 'value' attributes")
             return None
         if not answer['success']:
             error("Command not succeeded, because of %s" % answer['value'])
             return None
         return answer['value']
+
+
+class CommandsHandler(object):
+    STATUS_FIELD = 'status'
+    VALUE_FIELD = 'value'
+
+    def __init__(self, command_handling_url):
+        self._command_handling_socket = SafeServerZMQSocket(zmq.Context(), ZMQ_REQUEST, command_handling_url)
+        self._massage_type_to_handlers = {}
+
+    def add_massage_handler(self, command_type, handler):
+        self._massage_type_to_handlers[command_type] = handler
+
+    def handle_commands(self):
+        if not zmq_poll(self._command_handling_socket):
+            error("Didn't get message")
+            return
+
+        msg = self._command_handling_socket.receive_json()
+
+        if Commander.COMMAND_TYPE_FIELD not in msg:
+            error("Massage invalid, please use 'type' attribute")
+            (status, value) = False, 'got-bad-message'
+        elif msg[Commander.COMMAND_TYPE_FIELD] not in self._massage_type_to_handlers:
+            error("I don't know the type '%s', are you crazy?" % msg[Commander.COMMAND_TYPE_FIELD])
+            (status, value) = False, 'got-bad-message-type'
+        else:
+            info("Got 'get-configuration' commands.")
+            (status, value) = self._massage_type_to_handlers[
+                msg[Commander.COMMAND_TYPE_FIELD]](**msg[Commander.PARAMETERS_FIELD])
+
+        self._command_handling_socket.send_json({self.STATUS_FIELD: status, self.VALUE_FIELD: value})
