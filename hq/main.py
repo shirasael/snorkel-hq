6b2e18e0f56dc23dfbc88990f163941e20ba258f
Netanel Revah <netanelrevah@outlook.com>
2015-08-07 17:16:51 +0300

    create file for repository

--- hq/main.py
+++ hq/main.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 from logbook import info
 
 from hq.nice import zmq_poll, ZMQ_REPLY, Commander, CommandsHandler, SafeServerZMQSocket
-from hq.components import Repository
+from hq.repository import Repository
 from hq.agent import AgentCommander, SnorkelAgent
 
 
